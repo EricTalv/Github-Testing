981d3e1bfd34de2f39f14f4a5386e567585d49c2
Oliver Kilk <oliver.kilk@tptlive.ee>
2018-04-03 09:48:01 +0300

    Added 2 Buttons

--- Github-Testing/MainActivity.cs
+++ Github-Testing/MainActivity.cs
@@ -4,7 +4,9 @@ using Android.OS;
 
 namespace Github_Testing
 {
-    [Activity(Label = "Github_Testing", MainLauncher = true)]
+    [Activity(Icon = "@drawable/ic_launcher", Label = "MAGIC 8BALL",
+       MainLauncher = true, Theme = "@android:style/Theme.Material.NoActionBar",
+         ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
